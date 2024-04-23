59fa29291aaaa652789b5dfde84149a3edbc02c9
Alcides Junior <101275506+allsides2@users.noreply.github.com>
2024-04-23 02:08:31 -0300

    Update sql.sql
    add mana

--- sql.sql
+++ sql.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `supernatural` (
   `identifier` varchar(50) DEFAULT NULL,
   `class` varchar(5000) DEFAULT NULL,
+  `mana` INT DEFAULT 0,
   `role` int DEFAULT NULL,
   `transform` VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (`identifier`)
